af6c2f5a39458f37e3ebbdcc5cc7a7d82ea703b2
Shinya Nakamura <100843119+Shinya-Labs@users.noreply.github.com>
2025-10-22 18:08:10 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,36 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  security_groups = [aws_security_group.ssh_access.name]
 
   tags = {
     Name = var.instance_name
   }
 }
+
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+  description = "Allow SSH access"
+  vpc_id      = null # デフォルトVPCを使用する場合
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+security_groups = [aws_security_group.ssh_access.name]
